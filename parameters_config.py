7fecba021ad2b772a08211b1d25e8d291e891938
KingSongtao <david.songtao@hotmail.com>
2024-07-11 16:19:34 +0800

    路径换成绝对路径

--- parameters_config.py
+++ parameters_config.py
@@ -6,16 +6,16 @@ import torch
 
 class ParametersConfig:
     def __init__(self):
-        self.train_txt = r"../local_data/medical_train.txt"
-        self.valid_txt = r"../local_data/medical_valid.txt"
-        self.train_pkl = r"../local_data/medical_train.pkl"
-        self.valid_pkl = r"../local_data/medical_valid.pkl"
+        self.train_txt = r"D:\Medical_QA_System_GPT2\local_data\medical_train.txt"
+        self.valid_txt = r"D:\Medical_QA_System_GPT2\local_data\medical_valid.txt"
+        self.train_pkl = r"D:\Medical_QA_System_GPT2\local_data\medical_train.pkl"
+        self.valid_pkl = r"D:\Medical_QA_System_GPT2\local_data\medical_valid.pkl"
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.batch_size = 4
-        self.tokenizer_model = r"../models/bert-base-chinese"
-        self.model = ''
-        self.model_config = r'./models/gpt2/config.json'
-        self.save_model_path = r'./models/save_model'
+        self.tokenizer_model = r"D:\Medical_QA_System_GPT2\models\bert-base-chinese"
+        self.gpt2_pretrained_model = ''
+        self.gpt2_model_config = r'D:\Medical_QA_System_GPT2\models\gpt2\config.json'
+        self.save_model_path = r'D:\Medical_QA_System_GPT2\models\save_model'
         self.epochs = 4
         self.gradient_accumulation_steps = 4
         self.eps = 1.0e-09  # 为了增加数值计算的稳定性二加到分母里的项，防止在实现中分母为0
