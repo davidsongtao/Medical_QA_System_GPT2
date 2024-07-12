b5c48d7859507d734a344b06bce26a7d5cbbcab0
KingSongtao <david.songtao@hotmail.com>
2024-07-12 20:30:01 +0800

    更新了batch_size和epochs

--- parameters_config.py
+++ parameters_config.py
@@ -11,12 +11,12 @@ class ParametersConfig:
         self.train_pkl = r"D:\Medical_QA_System_GPT2\local_data\medical_train.pkl"
         self.valid_pkl = r"D:\Medical_QA_System_GPT2\local_data\medical_valid.pkl"
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.batch_size = 4
+        self.batch_size = 5
         self.tokenizer_model = r"D:\Medical_QA_System_GPT2\models\bert-base-chinese"
         self.gpt2_pretrained_model = ''
         self.gpt2_model_config = r'D:\Medical_QA_System_GPT2\models\gpt2\config.json'
         self.save_model_path = r'D:\Medical_QA_System_GPT2\models\save_model'
-        self.epochs = 4
+        self.epochs = 30
         self.gradient_accumulation_steps = 4
         self.eps = 1.0e-09  # 为了增加数值计算的稳定性二加到分母里的项，防止在实现中分母为0
         self.warmup_steps = 1000
