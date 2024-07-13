a74cb3a903031ffcb0091b9d38e65cdf2ec041b5
KingSongtao <david.songtao@hotmail.com>
2024-07-13 11:43:34 +0800

    更新了best_model

--- parameters_config.py
+++ parameters_config.py
@@ -16,7 +16,7 @@ class ParametersConfig:
         self.gpt2_pretrained_model = ''
         self.gpt2_model_config = r'D:\Medical_QA_System_GPT2\models\gpt2\config.json'
         self.save_model_path = r'D:\Medical_QA_System_GPT2\models\save_model'
-        self.epochs = 30
+        self.epochs = 50
         self.gradient_accumulation_steps = 4
         self.eps = 1.0e-09  # 为了增加数值计算的稳定性二加到分母里的项，防止在实现中分母为0
         self.warmup_steps = 1000
@@ -24,3 +24,5 @@ class ParametersConfig:
         self.lr = 2.6e-5
         self.max_grad_norm = 2.0
         self.loss_step = 10  # 设置多少步打印一次损失值
+        self.train_log_path = r"D:\Medical_QA_System_GPT2\logs\train_log.txt"
+        self.best_model = r"D:\Medical_QA_System_GPT2\models\save_model\best_model_9"
